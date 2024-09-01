86287b9dc764353b1f853cac8ca2daef093e99ff
Karunakar31 <talatamkarun@gmail.com>
2024-09-01 20:03:43 +0530

    solved server errors

--- server/index.js
+++ server/index.js
@@ -1,21 +1,29 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const connectDB = require("./database");
 const routes = require("./routes/index");
 
 connectDB();
 
 const app = express();
-let port = process.env.PORT;
-if (port == null || "") {
-  port = 9000;
-}
+let port = process.env.PORT || 9000;
 
-app.use(cors());
+
+const corsOptions = {
+  origin: 'https://quiz-builder-front.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
+};
+
+
+app.use(cors(corsOptions));
+
+
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
-
 app.use(routes());
-
 
 app.listen(port, () => {
   console.log("Server started at port: " + port);
